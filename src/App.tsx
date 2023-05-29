eeb59fc7f31a59fee19a0ec432770746336dd0bb
Mendoza <smendoza171@gmail.com>
2023-05-29 07:46:54 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./scss/App.css";
+import Component from "./components/Component/Component";
+function App() {
+  return (
+    <div className="App">
+      <Component />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Component/Component.tsx
@@ -0,0 +1,11 @@
+
+
+import React from 'react';
+import "./Component.scss";
+
+
+function Component() {
+  return <div>Component</div>;
+}
+
+export default Component;
