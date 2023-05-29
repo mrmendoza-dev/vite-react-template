7a67df5753d1aff8ff298b10e4afca7f0f3680e5
Mendoza <smendoza171@gmail.com>
2023-05-29 07:52:44 -0700

    added icons

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+## TODO
+- Add FontAwesome with icons file
+- 
---
+++ src/assets/icons.ts
@@ -0,0 +1,61 @@
+import {
+  faGithub,
+  faLinkedin,
+  faMedium,
+  faYoutube,
+  faCodepen,
+  faTwitter,
+  faDev,
+  faDribbble,
+  faBehance,
+} from "@fortawesome/free-brands-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEnvelope,
+  faPhone,
+  faShareNodes,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFileArrowDown,
+  faRotateRight,
+  faBars,
+  faShuffle,
+  faCircleNotch,
+} from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import {
+  faXmark,
+  faMaximize,
+  faMobileScreen,
+  faDesktop,
+} from "@fortawesome/free-solid-svg-icons";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+
+export const icons = {
+  faGithub,
+  faLinkedin,
+  faMedium,
+  faYoutube,
+  faCodepen,
+  faLocationDot,
+  faEnvelope,
+  faPhone,
+  faFileArrowDown,
+  faAngleRight,
+  faAngleLeft,
+  faXmark,
+  faSun,
+  faMoon,
+  faMobileScreen,
+  faDesktop,
+  faMaximize,
+  faRotateRight,
+  faTwitter,
+  faDev,
+  faDribbble,
+  faBehance,
+  faShareNodes,
+  faBars,
+  faShuffle,
+  faCircleNotch,
+};
