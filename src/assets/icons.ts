53214aee451c428b4f6b90f33fa16fc33bf6bc1c
Steven Mendoza <smendoza171@gmail.com>
2024-12-30 18:00:53 -0800

    organized icons

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,119 +1,214 @@
-import {
-  faAppStoreIos,
-  faBitbucket,
-  faBitcoin,
-  faDiscord,
-  faEthereum,
-  faFacebook,
+// Social Media & Brand Icons
+export {
   faGithub,
-  faGooglePlay,
-  faInstagram,
-  faLinkedin,
+  faGoogle,
   faMedium,
+  faYoutube,
+  faTwitter,
+  faXTwitter,
+  faFacebook,
   faReddit,
   faTelegram,
+  faDiscord,
+  faLinkedin,
   faTiktok,
-  faTwitter,
-  faYoutube,
+  faInstagram,
+  // App Stores
+  faAppStoreIos,
+  faGooglePlay,
+  // Cryptocurrencies
+  faBitcoin,
+  faEthereum,
 } from "@fortawesome/free-brands-svg-icons";
 
-import {
-  faBookmark as faBookmarkEmpty,
-  faHeart as faHeartEmpty,
-  faLightbulb as faLightbulbEmpty,
-  faStar as faStarEmpty,
-} from "@fortawesome/free-regular-svg-icons";
-import {
+// Navigation & UI Controls
+export {
+  // Carets & Angles
+  faCaretDown,
+  faCaretLeft,
+  faCaretRight,
+  faCaretUp,
+  faAngleDown,
+  faAngleLeft,
+  faAngleRight,
+  faAngleUp,
+  faArrowRight,
+  faArrowsLeftRight,
+  faAnglesRight,
+  // Basic UI
+  faBars,
+  faEllipsis,
+  faXmark,
+  faMaximize,
+  faMinimize,
+  faSearch,
+  faMagnifyingGlass,
+  // Responsive
+  faMobileScreen,
+  faDesktop,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Actions & Operations
+export {
+  // File Operations
+  faFileArrowDown,
+  faFileArrowUp,
+  faFileImage,
+  faFileLines,
+  faFileSignature,
+  faFile,
+  faPaste,
+  faDownload,
+  faClipboardList,
+  // Edit Actions
+  faPenToSquare,
+  faTrash,
+  faClone,
+  faCopy,
+  // Add/Remove
+  faPlus,
+  faMinus,
+  faPlusCircle,
+  faMinusCircle,
+  faPlusSquare,
+  faMinusSquare,
+  faCirclePlus,
+  faSquarePlus,
+  // Other Actions
+  faRotateRight,
+  faRefresh,
+  faArrowsRotate,
+  faShare,
+  faShareNodes,
+  faUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Media Controls
+export {
+  faCirclePlay,
+  faCirclePause,
+  faPlay,
+  faPause,
+  faBackward,
+  faForward,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Status & Information
+export {
+  faSpinner,
+  faCircleNotch,
+  faCheck,
+  faInfo,
+  faCircleInfo,
+  faQuestion,
+  faFilter,
+  faFilterCircleXmark,
+  faEye,
+  faEyeSlash,
+  faFireFlameCurved,
+  faFire,
+} from "@fortawesome/free-solid-svg-icons";
+
+// User Interface Elements
+export {
+  // Settings & Tools
+  faCog,
+  faSliders,
+  faScrewdriverWrench,
+  // Dashboard Elements
+  faGaugeHigh,
+  faDashboard,
+  faChartLine,
+  faChartPie,
+  // Lists & Layout
+  faList,
+  faLayerGroup,
+  faSquare,
+  faSquareFull,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Communication & Contact
+export {
+  faMicrophone,
+  faPaperPlane,
+  faEnvelope,
+  faPhone,
+  faLocationDot,
+  faLocationPin,
+  faPaperclip,
+} from "@fortawesome/free-solid-svg-icons";
+
+// User & Account
+export {
+  faUser,
+  faUsers,
+  faCircleUser,
+  faWallet,
+  faBell,
+  faArrowRightFromBracket,
+  faRightFromBracket,
+  faPowerOff,
+  faLock,
+  faUserSlash,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Business & Commerce
+export {
+  faBagShopping,
+  faReceipt,
+  faDollarSign,
+  faBuildingColumns,
+  faLifeRing,
+  faCertificate,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Content & Documents
+export {
+  faBookOpen,
+  faNoteSticky,
+  faNewspaper,
+  faInbox,
+  faBoxArchive,
+  faCode,
+  faTerminal,
+  faVideo,
+  faBrain,
+  faSailboat,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Misc Utilities
+export {
+  faGlobe,
+  faCalendar,
+  faClock,
+  faCalculator,
+  faQrcode,
+  faLink,
+  faSortUp,
+  faSortDown,
+  faRightLeft,
+  faHouse,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Theme Icons
+export {
+  faSun,
+  faMoon,
+  faCircleHalfStroke,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Toggle State Icons
+export {
+  faStar as faStarFilled,
   faBookmark as faBookmarkFilled,
   faHeart as faHeartFilled,
   faLightbulb as faLightbulbFilled,
-  faStar as faStarFilled,
-} from "@fortawesome/free-solid-svg-icons";
-
-import {
-  faAngleDown,
-  faAngleLeft,
-  faAngleRight,
-  faAngleUp,
-  faBars,
-  faBell,
-  faCaretDown,
-  faCaretLeft,
-  faCaretRight,
-  faCaretUp,
-  faCheck,
-  faCircleNotch,
-  faCog,
-  faEllipsis,
-  faEye,
-  faFileSignature,
-  faFire,
-  faHouse,
-  faLayerGroup,
-  faMagnifyingGlass,
-  faMoon,
-  faPlus,
-  faSearch,
-  faSliders,
-  faSpinner,
-  faSun,
-  faUser,
-  faUserSlash,
-  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 
 export {
-  faGithub,
-  faMedium,
-  faYoutube,
-  faTwitter,
-  faBitcoin,
-  faEthereum,
-  faFacebook,
-  faReddit,
-  faTelegram,
-  faBitbucket,
-  faDiscord,
-  faLinkedin,
-  faTiktok,
-  faInstagram,
-  faAppStoreIos,
-  faGooglePlay,
-  faStarFilled,
-  faBookmarkFilled,
-  faHeartFilled,
-  faLightbulbFilled,
-  faStarEmpty,
-  faBookmarkEmpty,
-  faHeartEmpty,
-  faLightbulbEmpty,
-  faAngleUp,
-  faAngleDown,
-  faAngleRight,
-  faAngleLeft,
-  faCaretUp,
-  faCaretDown,
-  faCaretRight,
-  faCaretLeft,
-  faSun,
-  faMoon,
-  faBars,
-  faEllipsis,
-  faHouse,
-  faSearch,
-  faUser,
-  faCog,
-  faBell,
-  faPlus,
-  faXmark,
-  faCheck,
-  faSliders,
-  faMagnifyingGlass,
-  faEye,
-  faLayerGroup,
-  faFire,
-  faCircleNotch,
-  faSpinner,
-  faUserSlash,
-  faFileSignature,
-};
+  faStar as faStarEmpty,
+  faBookmark as faBookmarkEmpty,
+  faHeart as faHeartEmpty,
+  faLightbulb as faLightbulbEmpty,
+  faSquare as faSquareEmpty,
+} from "@fortawesome/free-regular-svg-icons";
