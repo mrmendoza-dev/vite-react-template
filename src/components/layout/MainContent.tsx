00e5c0d3fdcbc7053ee5008d4f5db7b06c8b2291
Steven Mendoza <smendoza171@gmail.com>
2025-01-08 00:03:58 -0800

    minor change to main height

--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -3,7 +3,7 @@ import { HomePage } from "@/pages/HomePage";
 
 export const MainContent = () => {
   return (
-    <main className="flex-1 w-0 min-w-0 h-[calc(100vh-4rem)] overflow-auto p-2 px-4 bg-background">
+    <main className="flex-1 w-0 min-w-0 h-[calc(100vh)] overflow-auto p-2 px-4 bg-background">
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
